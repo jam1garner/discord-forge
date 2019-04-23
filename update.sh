bfb5ba23b0c0fca1ee2d57d65cea78486d49e7dd
root <root@jam1.re>
2019-04-23 22:59:25 +0000

    Make it only print update success message on success

--- update.sh
+++ update.sh
@@ -1,20 +1,20 @@
-rm -rf paramxml/
-wget https://github.com/BenHall-7/paracobNET/releases/download/1.99/Release.zip > /dev/null
-unzip Release.zip -d paramxml > /dev/null
-rm Release.zip
-echo "Installed ParamXML 1.99"
+rm -rf paramxml/ &&\
+	wget https://github.com/BenHall-7/paracobNET/releases/download/1.99/Release.zip > /dev/null &&\
+	unzip Release.zip -d paramxml > /dev/null &&\
+	rm Release.zip &&\
+	echo "Installed ParamXML 1.99"
 
-wget https://raw.githubusercontent.com/ultimate-research/param-labels/master/ParamLabels.csv
-mv ParamLabels.csv paramxml/netcoreapp2.1/
-echo "Installed latest ParamLabels.csv"
+wget https://raw.githubusercontent.com/ultimate-research/param-labels/master/ParamLabels.csv &&\
+	mv ParamLabels.csv paramxml/netcoreapp2.1/ &&\
+	echo "Installed latest ParamLabels.csv"
 
-rm -rf vgaudio/
-wget https://ci.appveyor.com/api/buildjobs/wa2ie68wd0eq51sw/artifacts/VGAudioCli.zip > /dev/null
-unzip VGAudioCli.zip -d vgaudio > /dev/null
-rm VGAudioCli.zip
-echo "Installed VGAudio"
+rm -rf vgaudio/ &&\
+	wget https://ci.appveyor.com/api/buildjobs/wa2ie68wd0eq51sw/artifacts/VGAudioCli.zip > /dev/null &&\
+	unzip VGAudioCli.zip -d vgaudio > /dev/null &&\
+	rm VGAudioCli.zip &&\
+	echo "Installed VGAudio"
 
-wget https://raw.githubusercontent.com/BenHall-7/msc_labels/master/mscinfo.xml
-cp mscinfo.xml mscdec/
-mv mscinfo.xml msclang/
-echo "Installed latest mscinfo.xml"
+wget https://raw.githubusercontent.com/BenHall-7/msc_labels/master/mscinfo.xml &&\
+	cp mscinfo.xml mscdec/ &&\
+	mv mscinfo.xml msclang/ &&\
+	echo "Installed latest mscinfo.xml"
