7b3fef5c26b897eaa22b1a4a344ccf1bb2703436
BenHall-7 <benjaminjahall@gmail.com>
2020-04-29 03:55:58 -0400

    Touchups

--- src/converter/param.rs
+++ src/converter/param.rs
@@ -1,7 +1,6 @@
 use super::error::ConvertError;
 use std::io::Seek;
 use std::path::{Path, PathBuf};
-use std::process::Command;
 use super::{Converter, Convert};
 use prc::xml;
 
@@ -19,9 +18,9 @@ impl Converter for ParamConverter {
     fn convert_from(&self, path: &Path, _: Option<&str>) -> Result<PathBuf, ConvertError> {
         let mut outpath = PathBuf::from(path);
         outpath.set_extension("xml");
-        let mut writer = std::io::BufWriter::new(std::fs::File::create(outpath)?);
+        let mut writer = std::io::BufWriter::new(std::fs::File::create(&outpath)?);
         xml::write_xml(&prc::open(path)?, &mut writer)
-            .or_else(|e| Err(ConvertError::param(format!("{:?}", e).as_ref())));
+            .or_else(|e| Err(ConvertError::param(format!("{:?}", e).as_ref())))?;
         Ok(outpath)
     }
 
@@ -30,12 +29,12 @@ impl Converter for ParamConverter {
         outpath.set_extension("prc");
         let mut reader = std::io::BufReader::new(std::fs::File::open(path)?);
         match xml::read_xml(&mut reader) {
-            Ok(p) => prc::save(outpath, &p)?,
+            Ok(p) => prc::save(&outpath, &p)?,
             Err(e) => {
                 reader.seek(std::io::SeekFrom::Start(0))?;
                 return Err(ConvertError::param(
                     format!(
-                        "{}\n{:?}",
+                        "{}{:?}",
                         xml::get_xml_error(&mut reader, e.start, e.end)?,
                         e.error
                     ).as_ref()
