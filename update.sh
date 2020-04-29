3468ccc9cfbc50c9761a7a701fae4e9b33fabfc6
BenHall-7 <benjaminjahall@gmail.com>
2020-04-29 04:36:30 -0400

    Update update.sh to not retrieve ParamXML; change param labels path

--- update.sh
+++ update.sh
@@ -1,12 +1,6 @@
-rm -rf paramxml/ &&\
-	wget https://github.com/BenHall-7/paracobNET/releases/download/1.99/Release.zip > /dev/null &&\
-	unzip Release.zip -d paramxml > /dev/null &&\
-	rm Release.zip &&\
-	echo "Installed ParamXML 1.99"
-
 wget https://raw.githubusercontent.com/ultimate-research/param-labels/master/ParamLabels.csv &&\
-	mv ParamLabels.csv paramxml/netcoreapp2.1/ &&\
-	echo "Installed latest ParamLabels.csv"
+	mv ParamLabels.csv param_labels.csv &&\
+	echo "Installed param labels"
 
 rm -rf vgaudio/ &&\
 	wget https://ci.appveyor.com/api/buildjobs/6v3widme4hdqqwc7/artifacts/VGAudioCli.zip > /dev/null &&\
